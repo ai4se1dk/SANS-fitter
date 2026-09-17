0f6a39d9f4fe51e49de65b0dfd62ce92e29cd250
rozyczko <piotr.rozyczko@gmail.com>
2026-09-17 13:16:28 +0200

    preparation for the release

--- src/sans_fitter/__init__.py
+++ src/sans_fitter/__init__.py
@@ -2,7 +2,7 @@
 SANS Model Fitter - A flexible template for fitting SANS data with SasModels
 """
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 from . import examples
 from . import inversion as pr_inversion
